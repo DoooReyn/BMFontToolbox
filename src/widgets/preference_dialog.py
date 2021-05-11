645a74e7cb53b62748815dee78d3ff3db83477be
DoooReyn <jl88744653@gmail.com>
2021-05-11 18:31:11 +0800

    添加首选项对话框

---
+++ src/widgets/preference_dialog.py
@@ -0,0 +1,10 @@
+from PySide6.QtWidgets import QDialog
+
+
+class PreferenceDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.setup_ui()
+
+    def setup_ui(self):
+        pass
